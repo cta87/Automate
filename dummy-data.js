5ed4f7e322e6e4d022e1af9ac17d30cc0e570d85
Cameron Armstrong <carmstrong@windowslive.com>
2020-01-24 15:48:35 +1300

    Added dummy-data

---
+++ dummy-data.js
@@ -0,0 +1,7 @@
+let dummyData = [
+    {name: 'test1', result: 'Pass', description: "This describes test 1", log: ['This is a longer log to test the full width of the table', "log2", 'log3','log4'], note: '', review: false},
+    {name: 'test4', result: 'Pass', description: "This describes test 1", log: ['log1', "log2", 'log3','log4'], note: '', review: false},
+    {name: 'test2', result: 'Fail', description: "This describes test 2", log: ['log1', "log2", 'log3'], note: 'This has a note', review: true},
+    {name: 'test3', result: 'Error', description: "This describes test 3", log: ['log1', "log2", 'log3']},
+
+];
